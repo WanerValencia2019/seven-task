1bb7cb169843901e1ec3f714c9a2900014880300
WnaesVlamro <valenciawaner@gmail.com>
2020-06-03 05:14:22 -0500

    Updates

---
+++ precache-manifest.f2eba71be3da8676d0ff194b4e99a297.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e192b4b6c0f28c7aa8b05923edda74af",
+    "url": "/seven-task/index.html"
+  },
+  {
+    "revision": "d608d500d311aa3a84e2",
+    "url": "/seven-task/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "22f9cc4e1c57ef58e6a5",
+    "url": "/seven-task/static/js/2.f1e78e4e.chunk.js"
+  },
+  {
+    "revision": "1476762afe1aa8b2e1b2ce657d32eb83",
+    "url": "/seven-task/static/js/2.f1e78e4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d608d500d311aa3a84e2",
+    "url": "/seven-task/static/js/main.61329d76.chunk.js"
+  },
+  {
+    "revision": "98c0492a32362833c407",
+    "url": "/seven-task/static/js/runtime-main.f4baa862.js"
+  }
+]);
